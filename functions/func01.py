819a48ee6ac00542d6c8a9de7c260fd508b14b1c
Admin <admin@admins-imac-2.home>
2022-06-15 18:00:53 +1000

    p

---
+++ functions/func01.py
@@ -0,0 +1,51 @@
+# def max_two(x,y):
+#     if x > y:
+#         return x
+#     else: 
+#         return y
+
+# def max_three(x, y, z):
+#     return max_two(x, max_two(y,z))
+
+# print(max_three(34,45,56))
+
+
+
+# def add_numbers(x):
+#     count = 0
+#     for num in x:
+#         count = count + num
+#     return count
+# list_num = [50,50,50]
+# print(add_numbers(list_num))
+
+# def multiply_numbers(x):
+#     count = 1
+#     for num in x:
+#         count = count * num
+#     return count
+# list_num = [5,5,5,5]
+# print(multiply_numbers(list_num))
+
+
+# txt = "Hello World"[::-1]
+# print(txt)
+
+# string = 'Admins-iMac-2'
+# def reverse_string(string):
+#     return string [::-1]
+
+# string = 'Admins-iMac-2'
+# def reverse_string(string):
+#     new_string = ''
+#     index = len(string)
+#     while index > 0:
+#         new_string += string[index - 1]
+#         index = index - 1
+#     return new_string
+
+# print(reverse_string(string))
+
+# Source: https://www.w3resource.com/python-exercises/python-functions-exercises.php
+
+
